eb4fda51a7af5ff2129bdef78f985be18cf516f0
NathanJMarcos <31256915+NathanJMarcos@users.noreply.github.com>
2017-09-27 20:05:18 -0700

    Update PriceListItem.h

--- PriceListItem.h
+++ PriceListItem.h
@@ -1,18 +1,32 @@
+//***********************
+//Nathan Marcos (Section 8)
+//Cubi Decastro (Section 4)
+//CPSC 131
+//Project 1
+//***********************
+
 #pragma once
 #include <string>
+#include <stdexcept>
+#include "PriceListItem.h"
 
 using namespace std;
 
-class PriceListItem {
-public:
-	PriceListItem();
-	PriceListItem(const string &itemName, const string &code, double price, bool taxable);
-	string getItemName();
-	string getCode();
-	double getPrice();
-	bool isTaxable();
-private:
-	// any private member variables and methods go here
-	// TO BE COMPLETED
+//Description for each function in the .cpp file
 
+class PriceList {
+public:
+
+	void createPriceListFromDatafile(string filename);
+	void addEntry(string itemName, string code, double price, bool taxable);
+	bool isValid(string code) const;
+	PriceListItem getItem(string code) const;
+
+	PriceList::PriceList();
+	PriceList::PriceList(const PriceList &priceList);
+	PriceList& PriceList::operator=(const PriceList& priceList);
+
+private:
+	PriceListItem *list;
+	int index;
 };
