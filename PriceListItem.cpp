236d5e8e30c7da59bee4793836a0a197b74af14f
NathanJMarcos <31256915+NathanJMarcos@users.noreply.github.com>
2017-09-27 20:04:09 -0700

    Update PriceListItem.cpp

--- PriceListItem.cpp
+++ PriceListItem.cpp
@@ -1,27 +1,49 @@
+//***********************
+//Nathan Marcos (Section 8)
+//Cubi Decastro (Section 4)
+//CPSC 131
+//Project 1
+//***********************
+
 #include "PriceListItem.h"
 
+//Constructor for a PriceListItem class object
 PriceListItem::PriceListItem(const string &itemName, const string &code, double price, bool taxable) {
-	// TO BE COMPLETED
+	_itemName = itemName;
+	_code = code;
+	_price = price;
+	_taxable = taxable;
 }
 
+//Default constructor for a PriceListItem class object
 PriceListItem::PriceListItem() {
-	// TO BE COMPLETED
-
+	_itemName = " ";
+	_code = " ";
+	_price = 0;
+	_taxable = false;
 }
 
+//Returns the item name
 string PriceListItem::getItemName() {
-	// TO BE COMPLETED
+	return _itemName;
 }
 
+//Returns the item code
 string PriceListItem::getCode() {
-	// TO BE COMPLETED
+	return _code;
 }
 
+//Returns the item price
 double PriceListItem::getPrice() {
-	// TO BE COMPLETED
+	return _price;
 }
 
+//Returns true if the item is taxable and false if the item is not taxable
 bool PriceListItem::isTaxable() {
-	// TO BE COMPLETED
+	return _taxable;
 }
 
+//Sets the price of the item
+void PriceListItem::setPrice(double billPrice) {
+	_price = billPrice;
+}
